f681912ee3d30823d8e73a8506decd277100072a
juel-47 <juel@inoodex.com>
2026-03-29 17:30:07 +0600

    feat: add configuration file for spatie/laravel-backup

--- config/backup.php
+++ config/backup.php
@@ -20,7 +20,7 @@ return [
                 'include' => [
                     // base_path(),
                     // storage_path('app/public'),
-                    // base_path('public'),
+                    base_path('public/uploads'),
                 ],
 
                 /*
